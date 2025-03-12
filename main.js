7539fa91e6d1acea5210e20b2c3623b05f538654
mahdi_dev <hadjidjmahdi@gmail.com>
2025-03-12 14:26:29 +0100

    Update main.js

--- main.js
+++ main.js
@@ -7,37 +7,35 @@ galleryContainer.addEventListener('wheel', (e)=>{
     e.preventDefault();
     if(galleryContainer.clientWidth >= 800){
         galleryContainer.scrollLeft += (e.deltaY * 3)
-        isScrolled = true;
-        StopAutoScroll()
     }
     else{
         galleryContainer.scrollLeft += e.deltaY
-        isScrolled = true;
-        StopAutoScroll()
     }  
+    isScrolled = true;
+    StopAutoScroll()
 })
-if(galleryContainer.clientWidth <= 910){
+if(galleryContainer.clientWidth <= 800){
     backBtn.addEventListener('click', ()=>{
         galleryContainer.scrollBy({left:-100 , behavior:'smooth'})
         isScrolled = true;
-        StopAutoScroll()
+        resetScroll()
     })
     nextBtn.addEventListener('click', ()=>{
         galleryContainer.scrollBy({left:100 , behavior:'smooth'})
         isScrolled = true;
-        StopAutoScroll()
+        resetScroll()
     })
 }
 else{
     backBtn.addEventListener('click', ()=>{
         galleryContainer.scrollBy({left:-300 , behavior:'smooth'})
         isScrolled = true;
-        StopAutoScroll()
+        resetScroll()
     })
     nextBtn.addEventListener('click', ()=>{
         galleryContainer.scrollBy({left:300 , behavior:'smooth'})
         isScrolled = true;
-        StopAutoScroll()
+        resetScroll()
     })  
 }
 
@@ -67,4 +65,9 @@ function StopAutoScroll(){
     StopAutoScroll.timeout = setTimeout(()=>{
         isScrolled = false;
     },3000)
+}
+
+function resetScroll() {
+    isScrolled = true;
+    StopAutoScroll();
 }
